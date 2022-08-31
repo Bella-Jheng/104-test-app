c9af02097c273729a90968e9a7bf3cd35eb643e2
zhengyiting <bz850308@gmail.com>
2022-08-31 23:07:51 +0800

    8/31 change package.json scripts start, add -p $PORT

--- components/layout/MainNavigation.js
+++ components/layout/MainNavigation.js
@@ -1,4 +1,3 @@
-import Link from 'next/link';
 import classes from './MainNavigation.module.css';
 
 function MainNavigation() {
--- components/meetups/NewMeetupForm.js
+++ components/meetups/NewMeetupForm.js
@@ -1,4 +1,4 @@
-import { useRef, useState, useEffect } from "react";
+import { useRef, useState } from "react";
 
 import Card from "../ui/Card";
 import classes from "./NewMeetupForm.module.css";
